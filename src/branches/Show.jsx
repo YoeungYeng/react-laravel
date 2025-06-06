138f24728718aeb9855333afd2079907faefa20d
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-06-06 15:03:14 +0700

    commit from YOEUNG YENG

--- src/branches/Show.jsx
+++ src/branches/Show.jsx
@@ -94,7 +94,7 @@ const Show = () => {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Category
+                Brand
               </th>
               <th scope="col" className="px-6 py-3">
                 Status
